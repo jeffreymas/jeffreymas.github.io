b4d3d153016bf26fbefbaca05d19518e09813ecd
Jepri <55873259+jeffreymas@users.noreply.github.com>
2022-03-28 18:06:50 +0700

    Update form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,41 +1,3 @@
-// window.addEventListener("DOMcontentLoaded",function(){
-//   var form = document.getElementById("my-form");
-//   var status = document.getElementById("status");
-
-//   function success(){
-//     form.reset();
-//     status.classList.add('success');
-//     status.innerHTML = "Thanks for your submission!";
-//   }
-
-//   function error(){
-//     status.classList.add('error');
-//     status.innerHTML = "Oops! There was a problem submitting your form";
-//   }
-
-//   form.addEventListener("submit",function(ev){
-//     ev.preventDefault();
-//     var data = new FormData(form);
-//   });
-// });
-
-//   function ajax(method, url, data, success, error) {
-//     var xhr = new XMLHttpRequest();
-//     xhr.open(method, url);
-//     xhr.setRequestHeader("Accept", "application/json");
-//     xhr.onreadystatechange = function(){
-//       if(xhr.readyState !== XMLHttpRequest.DONE) return;
-//       if(xhr.status === 200){
-//        success(xhr.response, xhr.responseType);
-//       } else{
-//         error(xhr.status, xhr.response, xhr.responseType);
-//       }
-//     };
-//     xhr.send(data);
-//   }
-
-
-
     const scriptURL = 'https://script.google.com/macros/s/AKfycbyH4vcbmqwvkq3bUjKZMNlAzxjyfBnoGA_ocDm0k6om8P0abHUvoI5z2uY_IRTDkP1Wlg/exec'
     const form = document.forms['submit-to-google-sheet']
 
@@ -62,7 +24,7 @@
                 $(".alert").fadeTo(500, 0).slideUp(500, function(){
                     $(this).remove(); 
                 });
-            }, 25000);
+            }, 5000);
           
         })
         .catch(error =>{
